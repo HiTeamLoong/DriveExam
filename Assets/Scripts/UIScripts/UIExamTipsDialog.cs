c880f971b01d64c3a5bc77a348011a392c6e4c29
薛龙 <xuelong9009@qq.com>
2019-03-23 22:28:11 +0800

    [修改]修改帮助预制与帮助的实现方式

--- Assets/Scripts/UIScripts/UIExamTipsDialog.cs
+++ Assets/Scripts/UIScripts/UIExamTipsDialog.cs
@@ -7,14 +7,37 @@ public class UIExamTipsDialog : UIDialog
 {
     public Button btnClose;
 
-    public GameObject tipsAilishe;
-    public GameObject tipsDazhong;
+    public GameObject help_Ailishe;
+    public GameObject help_Dazhong;
+    public GameObject help_Ailishe2015;
+    public GameObject help_BentengB30;
     public override void OnCreate()
     {
         base.OnCreate();
-        tipsAilishe.SetActive(GameDataMgr.Instance.carType == CarType.AiLiShe);
-        tipsDazhong.SetActive(GameDataMgr.Instance.carType == CarType.DaZhong);
 
+        GameObject helpPrefab = null;
+        switch (GameDataMgr.Instance.carType)
+        {
+            case CarType.DaZhong:
+                helpPrefab = help_Dazhong;
+                break;
+            case CarType.AiLiShe:
+                helpPrefab = help_Ailishe;
+                break;
+            case CarType.AiLiShe2015:
+                helpPrefab = help_Ailishe2015;
+                break;
+            case CarType.BenTengB30:
+                helpPrefab = help_BentengB30;
+                break;
+            default:
+                break;
+        }
+        if (helpPrefab != null)
+        {
+            GameObject help = Instantiate(helpPrefab, transform);
+
+        }
         btnClose.onClick.AddListener(OnClickClose);
     }
 
